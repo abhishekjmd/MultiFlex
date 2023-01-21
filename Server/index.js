cf55be82a51fa0fe7d8c9de8e88c5f0bf1aa7a65
abhishek tiwari <89748622+abhishekjmd@users.noreply.github.com>
2023-01-21 20:13:26 +0530

    reinstalled the server

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,8 @@
 require('./config/db')
 const express = require('express')
+// const serverless = require('serverless-http')
 const app = express()
-const port = 4000
+// const port = process.env.PORT || 5000
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const movieRoute = require('./routes/moviesRoute')
@@ -16,9 +17,17 @@ require('dotenv').config();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use('/multiflex', movieRoute);
-app.use('/api/Top_playlist', topPlaylistRoute);
-app.use('/api/artist', artistRoute)
+// app.use('/.netlify/functions/api/Top_playlist', topPlaylistRoute);
+// app.use('/.netlify/functions/api/artist', artistRoute)
 // app.use(express.json());
 app.use(cors());
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/data', (req, res) => {
+    const data = {
+        message: 'hello this is the data you requested'
+    };
+    res.json(data);
+});
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+module.exports = app
+// module.exports.handler = serverless(app);
